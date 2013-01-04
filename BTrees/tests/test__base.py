b185b8f22c7c48b2bb564ce85a5f3f1adb3408f5
Tres Seaver <tseaver@palladion.com>
2013-01-04 18:45:43 -0500

    Try fixing test failures on Windows:
    Avoid relying on overflows w.r.t. sys.maxint.

--- BTrees/tests/test__base.py
+++ BTrees/tests/test__base.py
@@ -2854,13 +2854,9 @@ class Test_helpers(unittest.TestCase):
             pass
 
     def test_to_int_w_overflow(self):
-        import sys
         from BTrees._base import to_int
         faux_self = object()
-        try:
-            self.assertRaises(TypeError, to_int, faux_self, sys.maxint + 1)
-        except AttributeError: #pragma NO COVER Py3k
-            pass
+        self.assertRaises(TypeError, to_int, faux_self, 2**64)
 
     def test_to_int_w_invalid(self):
         from BTrees._base import to_int
@@ -2896,18 +2892,9 @@ class Test_helpers(unittest.TestCase):
             pass
 
     def test_to_long_w_overflow(self):
-        import sys
         from BTrees._base import to_long
         faux_self = object()
-        try:
-            #self.assertRaises(ValueError, to_long, faux_self, sys.maxint + 1)
-            to_long(faux_self, sys.maxint + 1)
-        except ValueError:
-            pass
-        except AttributeError: #pragma NO COVER Py3k
-            pass
-        else:
-            assert 0, "No ValueError:  sys.maxint -> %s" % sys.maxint
+        self.assertRaises(ValueError, to_long, faux_self, 2**64)
 
     def test_to_long_w_invalid(self):
         from BTrees._base import to_long
