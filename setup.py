bb08936139ff25a8bb7fe82395924655f04cb9ed
Jason Madden <jamadden@gmail.com>
2018-08-09 17:18:11 -0500

    Back to development: 4.5.2
    [skip ci]

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 from __future__ import print_function
-version = '4.5.1'
+version = '4.5.2.dev0'
 
 import os
 import sys
