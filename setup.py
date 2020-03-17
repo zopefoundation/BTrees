ce69faf6d2648bdeab3022a9d2575a807db5c145
Jason Madden <jamadden@gmail.com>
2020-03-17 09:44:12 -0500

    Back to development: 4.7.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@
 #
 ##############################################################################
 from __future__ import print_function
-version = '4.7.0'
+version = '4.7.1.dev0'
 
 import os
 import sys
