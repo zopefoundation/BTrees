0b4b592819ff9effb90e5728c9c61f9fe51d7674
Jens Vagelpohl <jens@plyp.com>
2023-10-06 07:29:00 +0200

    - vb [ci skip]

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ from setuptools import find_packages
 from setuptools import setup
 from setuptools.command.build_ext import build_ext
 
-version = '5.1'
+version = '5.1.1.dev0'
 
 def _read(fname):
     here = os.path.abspath(os.path.dirname(__file__))
